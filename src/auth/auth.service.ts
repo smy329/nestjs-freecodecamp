56212526e4b922ae9c7c08a00ac59c02603f46c5
Syed Mohammad Yasir <smy329@gmail.com>
2023-11-21 15:03:24 +0600

    creating user api

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
 
 @Injectable({})
 export class AuthService {
-  signin() {
+  constructor(private prisma: PrismaService) {}
+
+  async signin(dto: AuthDto) {
     return { msg: 'From signup' };
   }
-  signup() {
-    return { msg: 'From signin' };
+  async signup(dto: AuthDto) {
+    const hash = await argon.hash(dto.password);
+    const user = await this.prisma.user.create({
+      data: {
+        email: dto.email,
+        hash,
+      },
+    });
+
+    delete user.hash; // this line will not show hash on response
+    return user;
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // with this line i am rehecting the field which is not declared in dto
+    }),
+  );
   await app.listen(3500);
 }
 bootstrap();
